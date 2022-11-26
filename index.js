ce81e34e1216830180981029c7eb25df41b9e69c
SaifullaJubair <saifullajubair6@gmail.com>
2022-11-26 10:24:01 +0600

    connection with db

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mobosell:YMlT3Xl0cgrFICaF@cluster0.mesrluc.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+   try {
+
+   }
+   finally { }
+}
+run().catch(console.log)
+
+
+
+
+
+
+
 app.get('/', async (req, res) => {
    res.send('mobo server running ')
 })
